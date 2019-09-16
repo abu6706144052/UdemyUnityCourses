cb26dfce9f3693bca43c5c3d86ee5362b332a5f5
abu6706144052 <aldibakar67@gmail.com>
2019-09-16 10:50:52 +0700

    commit

---
+++ UdemyCourses/Assets/CharacterHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CharacterHealth : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject charPrefab;
+
+	[SerializeField]
+	private float respawnTime = 5f;
+
+	private float nextSpawnTime;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Time.time >= nextSpawnTime)
+		{
+			nextSpawnTime = Time.time + respawnTime;
+
+			GameObject.Instantiate(charPrefab, transform.position, transform.rotation);
+		}
+    }
+}
